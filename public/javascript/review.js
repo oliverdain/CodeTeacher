ab61f5c4375606d385c54ffb9cc947eae2f49b6c
Oliver Dain <oliver@dains.org>
2013-09-22 19:34:27 -0700

    Got line number clicking and code splitting working.

--- public/javascript/review.js
+++ public/javascript/review.js
@@ -8,35 +8,88 @@ var CodeReview = function(code, $elem) {
 
   Rainbow.color(code, 'html', function(highlighted) {
     self.syntaxLines = highlighted.split('\n');
-    var codeBlock = self.createCodeBlock(self.syntaxLines, 0);
+    var codeBlock = self.createCodeBlock(0, self.syntaxLines.length);
     self.codeBlocks.push(codeBlock);
     $elem.append(codeBlock.$codeElem);
   });
 };
 
-CodeReview.prototype.createCodeBlock = function(codeLines, startNumber) {
+
+CodeReview.prototype.createCodeElem = function(startLine, endLine) {
+  console.assert(startLine >= 0);
+  console.assert(endLine > startLine);
+  console.assert(endLine <= this.syntaxLines.length);
+
   var $pre = $('<pre/>');
 
-  for (i = 0; i < codeLines.length; ++i) {
+  for (i = startLine; i < endLine; ++i) {
     var $numSpan = $('<span/>', {class: 'line-number'});
     $numSpan.append((i + 1).toString());
-    $numSpan.click(_.partial(this.onLineClick, i));
+    $numSpan.click(_.bind(this.onLineClick, this, i));
     $pre.append($numSpan);
-    $pre.append(codeLines[i]);
+    $pre.append(this.syntaxLines[i]);
     $pre.append('\n');
   }
 
+  return $pre;
+};
+
+CodeReview.prototype.createCodeBlock = function(startLine, endLine) {
   var block = {
-    startLine: startNumber,
-    comment: null,
-    $codeElem: $pre
+    startLine: startLine,
+    $comment: null,
+    $codeElem: this.createCodeElem(startLine, endLine)
   };
 
   return block;
 };
 
 CodeReview.prototype.onLineClick = function(lineNum) {
-  console.log('Clicked: ' + lineNum);
+  console.assert(lineNum >= 0);
+  console.assert(lineNum < this.syntaxLines.length);
+
+  // Frikin' JS doesn't have any built-in binary search algorithm, and
+  // underscore and jquery's versions don't take a function that does the
+  // comparison so we do things the slow way here.
+  //
+  // TODO(odain) Convert to binary search.
+  for (var idx = 0; idx < this.codeBlocks.length; ++idx) {
+    if (this.codeBlocks[idx].startLine >= lineNum) {
+      break;
+    }
+  }
+
+  if (idx === this.codeBlocks.length ||
+      this.codeBlocks[idx].startLine > lineNum) {
+    --idx;
+  }
+
+  $replaceCodeElem = 
+      this.createCodeElem(this.codeBlocks[idx].startLine, lineNum + 1);
+  this.codeBlocks[idx].$codeElem.replaceWith($replaceCodeElem);
+  this.codeBlocks[idx].$codeElem = $replaceCodeElem;
+
+  var $comment = $('<p>Hello World!</p>');
+
+  this.codeBlocks[idx].$codeElem.after($comment);
+  this.codeBlocks[idx].$comment = $comment;
+
+  if (lineNum + 1 >= this.syntaxLines.length) {
+    // They clicked the last line, so done.
+    return;
+  } else {
+    if (idx + 1 < this.codeBlocks.length) {
+      var endLine = this.codeBlocks[idx + 1].startLine;
+    } else {
+      var endLine = this.syntaxLines.length;
+    }
+
+    var newBlock = this.createCodeBlock(lineNum + 1, endLine);
+    this.codeBlocks[idx].$comment.after(newBlock.$codeElem);
+
+    this.codeBlocks.push(newBlock);
+    this.codeBlocks.sort(function(a, b) { return a.startLine - b.startLine; });
+  }
 };
 
 
